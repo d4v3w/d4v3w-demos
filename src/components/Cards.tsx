6f749681cb3f13a0eaf96af81fda4b8379923d70
Dave Williams <mail@d4v3w.com>
2023-10-07 15:35:58 +0100

    scroll to on page load

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,31 +3,32 @@ import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import styles from "./Cards.module.css";
 
+const cards: string[] = [
+  "Excepteur tempor cupidatat mollit sit consectetur veniam amet eu.",
+  "Aliqua aute eu reprehenderit adipisicing esse sunt id laborum magna sit aliquip.",
+  "Velit magna nostrud non minim fugiat eu aliquip proident elit culpa officia.",
+  "Commodo veniam voluptate incididunt Lorem deserunt ea tempor fugiat ipsum in.",
+  "Anim adipisicing est pariatur aliqua pariatur esse enim occaecat minim dolore nostrud.",
+  "Et non pariatur veniam velit dolore nulla ut anim cupidatat eiusmod.",
+  "Dolor aliqua et ex eiusmod nostrud officia fugiat aliqua est ut.",
+  "Quis dolore laboris eu anim pariatur laborum sint irure ea est aute minim in elit.",
+  "Nostrud qui qui elit eiusmod nulla.",
+  "Non labore velit amet occaecat magna officia aliqua Lorem ex veniam esse magna minim veniam.",
+];
+
+const content =
+  "Consectetur in quis cupidatat officia ullamco nisi nulla consequat fugiat sunt velit proident quis. Ut veniam fugiat tempor dolore aliquip eiusmod sit labore enim excepteur non. Officia proident nulla enim enim fugiat cillum eiusmod nulla esse. Eu ullamco sit nostrud adipisicing ea Lorem dolor minim voluptate.";
+
 export const Cards = () => {
-  const radios = useRef<HTMLInputElement[]>([]);
-  const cards: string[] = [
-    "Excepteur tempor cupidatat mollit sit consectetur veniam amet eu.",
-    "Aliqua aute eu reprehenderit adipisicing esse sunt id laborum magna sit aliquip.",
-    "Velit magna nostrud non minim fugiat eu aliquip proident elit culpa officia.",
-    "Commodo veniam voluptate incididunt Lorem deserunt ea tempor fugiat ipsum in.",
-    "Anim adipisicing est pariatur aliqua pariatur esse enim occaecat minim dolore nostrud.",
-    "Et non pariatur veniam velit dolore nulla ut anim cupidatat eiusmod.",
-    "Dolor aliqua et ex eiusmod nostrud officia fugiat aliqua est ut.",
-    "Quis dolore laboris eu anim pariatur laborum sint irure ea est aute minim in elit.",
-    "Nostrud qui qui elit eiusmod nulla.",
-    "Non labore velit amet occaecat magna officia aliqua Lorem ex veniam esse magna minim veniam.",
-  ];
+  const labels = useRef<HTMLLabelElement[]>([]);
 
   const URL_STATE_QUERY = "card";
   const SELECT_KEYS = ["enter", " "];
   const [focusedItem, setFocusedItem] = useState<EventTarget & Element>();
-
   const [query, setQuery] = useSearchParams();
+
   // Defaults to selecting first item
   const q = query.get(URL_STATE_QUERY) || "0";
-  const content =
-    "Consectetur in quis cupidatat officia ullamco nisi nulla consequat fugiat sunt velit proident quis. Ut veniam fugiat tempor dolore aliquip eiusmod sit labore enim excepteur non. Officia proident nulla enim enim fugiat cillum eiusmod nulla esse. Eu ullamco sit nostrud adipisicing ea Lorem dolor minim voluptate.";
-
   const scrollToItem = () => {
     if (focusedItem == null) return;
     focusedItem.scrollIntoView({
@@ -63,13 +64,23 @@ export const Cards = () => {
       const dataIndex: string = item.getAttribute("data-radio") || "";
       const index = parseInt(dataIndex);
       console.log("SELECT", index);
-      radios.current[index].checked = true;
-      updateQuery(`${index}`);
+      checkRadioButton(index);
     }
   };
 
+  const checkRadioButton = (index: number) => {
+    const children: HTMLCollection = labels.current[index].children;
+    for (const tag of Array.from(children)) {
+      if (tag.nodeName === "INPUT") {
+        const radio = tag as HTMLInputElement;
+        radio.checked = true;
+      }
+    }
+    updateQuery(`${index}`);
+  };
+
   useEffect(() => {
-    console.log(`use effect`);
+    console.log(`use effect 1`);
     scrollToItem();
   }, [focusedItem]);
 
@@ -80,6 +91,14 @@ export const Cards = () => {
     };
   });
 
+  useEffect(() => {
+    const defaultSelection = labels.current[parseInt(q)];
+    defaultSelection.scrollIntoView({
+      behavior: "smooth",
+      inline: "center",
+    });
+  }, []);
+
   return (
     <>
       <form className={styles.wrapper}>
@@ -88,6 +107,9 @@ export const Cards = () => {
           const isSelected = id.toString() === q;
           return (
             <label
+              ref={(el) => {
+                el != null ? labels.current.push(el) : labels.current.push();
+              }}
               onFocus={onFocus}
               key={item}
               htmlFor={id}
@@ -106,9 +128,6 @@ export const Cards = () => {
                 className={styles.checkbox}
                 onChange={onChange}
                 checked={id == q}
-                ref={(el) => {
-                  el != null ? radios.current.push(el) : radios.current.push();
-                }}
               />
               <h3 className={classnames(styles.title, styles.lineClamp)}>
                 {item}
