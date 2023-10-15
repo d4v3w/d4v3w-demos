2f31bbbba4273e615fc7da9ec435b37ff9e4dcd7
Dave Williams <mail@d4v3w.com>
2023-10-15 12:55:31 +0100

    Arrow adds smooth scoll to clicked href

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,10 +1,23 @@
 import styles from "./Arrow.module.css";
 
+const clickHandler = (href: string) => {
+  const anchor = document.querySelector(href);
+  anchor?.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
 export const Arrow = () => {
   return (
     <div className={styles.container}>
-      <a className={styles.link} href="#top">
+      <a
+        className={styles.link}
+        onClick={() => {
+          clickHandler("#top");
+        }}
+      >
         <span className={styles.text}>[Scroll Down]</span>
+        <span className={styles.arrow}></span>
       </a>
     </div>
   );
